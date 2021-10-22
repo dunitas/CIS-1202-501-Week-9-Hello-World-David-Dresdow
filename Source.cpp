f1395fbb6a9f6bf26cf35848173efee81a466239
dunitas <david.dresdow@hotmail.com>
2021-10-21 21:00:52 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+// David Dresdow
+// October 21,2021
+// Program Name - Hello World
+// This program will output Hello World on the screen
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World " << endl;
+
+	return 0;
+}
